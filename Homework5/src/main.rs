9709193db5525e57679d150935aa43b348b0dcd4
Jacob Rivera <edjacob25@gmail.com>
2019-05-10 17:46:10 -0500

    Added time to hw 5

--- Homework5/src/main.rs
+++ Homework5/src/main.rs
@@ -1,12 +1,13 @@
 #![feature(test)]
 extern crate rand;
+extern crate time;
 
 use rand::distributions::{Distribution, Uniform};
 use rand::Rng;
 use std::error::Error;
 use std::fs::File;
 use std::io::{Write, BufReader, BufRead};
-
+use std::ops::Sub;
 
 fn main() {
     println!("Hello, world!");
@@ -15,10 +16,14 @@ fn main() {
     // print_vec(&vec);
     // heapsort(&mut vec);
     // print_vec(&vec);
-
-    let mut vec = create_numbers_array_base_10(5);
+    let mut vec = create_numbers_array_base_10(9);
+    println!("Created numbers");
+    let time1 =time::now();
     mergesort(&mut vec);
-    print_vec(&vec);
+    //print_vec(&vec);
+    let time2 = time::now();
+    let duration = time2.sub(time1);
+    println!("The duration is {}", duration);
 
     //let vec = create_numbers_array_base_10(5);
     //let ordered = quicksort(vec);
